8e20a6f52259537eebf10c4e1b0ca4d405e26307
Meinke7 <carlos.meinke7@gmail.com>
2023-07-06 11:50:21 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle("light")
+  
+    
+    const img = document.querySelector("#profile img")
+  
+    
+  
+  }
